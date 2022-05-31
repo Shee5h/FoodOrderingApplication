c532ffed2721bc5787f085deda83ee0f94c61eee
Ana <pvz@mail.com>
2022-05-31 18:50:26 +0300

    back, front create

---
+++ backend/src/test/java/lt/vtmc/FOApp/MainTests.java
@@ -0,0 +1,13 @@
+package lt.vtmc.FOApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
